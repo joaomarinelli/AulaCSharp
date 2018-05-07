6a555e7c2e490ea552f312c067cec25f90542de9
Aluno <Aluno@DOSO125261>
2018-05-07 11:22:57 -0300

    alterando

--- Aulas/Aula0705_Console/Program.cs
+++ Aulas/Aula0705_Console/Program.cs
@@ -10,6 +10,17 @@ namespace Aula0705_Console
     {
         static void Main(string[] args)
         {
+            int a = 10;
+
+            Console.WriteLine("Hello World");
+            Console.WriteLine("a:" + a);
+
+            if (Console.CapsLock)
+                Console.WriteLine("CapsLock ativado");
+            else
+                Console.WriteLine("CapsLock desativado");
+
+            Console.ReadKey();
         }
     }
 }
